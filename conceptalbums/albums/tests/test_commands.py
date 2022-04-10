704da963e4fc0ea4e967280d11e2e66308baa9b4
Jérémy <jeremy.lixandre@gmail.com>
2022-04-10 15:29:04 +0200

    new: :white_check_mark: add test for command

---
+++ conceptalbums/albums/tests/test_commands.py
@@ -0,0 +1,15 @@
+import pytest
+
+from albums.models import Album, Track
+from albums.commands import create_album_from_musicbrainz_and_genius
+
+
+@pytest.mark.django_db
+def test_create_album_from_musicbrainz_and_genius():
+    # Vektor - Terminal Redux
+    mbid = "8c14bbdd-de87-48fa-8cc9-153c3d56583e"
+    create_album_from_musicbrainz_and_genius(mbid)
+    album = Album.objects.get(mbid=mbid)
+    assert album
+    assert album.artists
+    assert Track.objects.filter(album=album).count() == 10
